a6c83da1d08e06a408e99d897d4e4b2483749f42
AliyusUnderwood <158104889+AliyusUnderwood@users.noreply.github.com>
2024-07-21 17:56:15 -0700

    Going to start from scratch rather than using starting code. got skeleton done.

---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+const Contacts = () => {
+    return (
+        <div id="contacts" className="section">
+        <h1>Contacts</h1>
+        <p>Contact me if you're interested.</p>
+        </div>
+    );
+};
+
+export default Contacts;
---
+++ src/components/Experience.jsx
@@ -0,0 +1,12 @@
+const Experience = () => {
+    return (
+        <div id="experience" className="section">
+        <h1>Experience</h1>
+        <p>Here's an overview of my work experience in the field 
+        of web development.
+        </p>
+        </div>
+    );
+};
+
+export default Experience;
