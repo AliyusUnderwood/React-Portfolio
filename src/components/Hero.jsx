d92a81d75faa17e7df5d142f65c9888c88ff0b85
AliyusUnderwood <158104889+AliyusUnderwood@users.noreply.github.com>
2024-07-22 23:49:25 -0700

    added hero placeholder

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+    return (
+        <div id="experience" className="section">
+        <h1>Hero</h1>
+        <p>Here's a placeholder for the Hero of this portfolio.
+        </p>
+        </div>
+    );
+};
+
+export default Hero;
